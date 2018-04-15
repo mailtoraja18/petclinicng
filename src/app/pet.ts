2c3950aa01a2a127b311f5e82a65ce819fbb1ef6
rajaramkumar <mailtoraja18@gmail.com>
2018-04-15 19:39:19 -0400

    some more changes

--- src/app/pet.ts
+++ src/app/pet.ts
@@ -1,7 +1,63 @@
-export class Pet {
-  name : string;
-  category : string;
-  status : string;
-  info : string;
-  zip : number;
+export interface Pet {
+  category: string;
+  info: Info;
+  status: string;
+  zip: number;
+  contact: Contact;
 }
+export interface Info {
+  name: string;
+  photoUrls?: (string)[] | null;
+  tags?: (TagsEntity)[] | null;
+}
+export interface TagsEntity {
+  description?: string | null;
+  health?: string | null;
+  gihw?: string | null;
+  age?: string | null;
+  sex?: string | null;
+}
+export interface Contact {
+  phone: string;
+  email: string;
+  address: string;
+}
+
+
+
+
+/*
+  {
+    "category": "cat",
+    "info": {
+      "name": "Marbles",
+      "photoUrls": [
+        "todo"
+      ],
+      "tags": [
+        {
+          "description": "Domestic Short Hair"
+        },
+        {
+          "health": "Vaccinations up to date, spayed \/ neutered."
+        },
+        {
+          "good in home with": "other cats"
+        },
+        {
+          "age": "young"
+        },
+        {
+          "sex": "female"
+        }
+      ]
+    },
+    "status": "available",
+    "zip": 19702,
+    "contact": {
+      "phone": "895-514-0673",
+      "email": "hwimsett9@wordpress.com",
+      "address": "36399 Cody Court"
+    }
+  }
+  */
